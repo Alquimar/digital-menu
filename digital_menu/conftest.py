0650d96f3508cebf9dacd8cd271bd1aca800d9a8
Alquimar <alquimars@gmail.com>
2021-07-14 00:08:07 -0300

    Initial settings

---
+++ digital_menu/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from digital_menu.users.models import User
+from digital_menu.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
