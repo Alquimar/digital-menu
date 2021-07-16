fb966a468883cd14e80335453f6ba3f2217262ce
Alquimar <alquimars@gmail.com>
2021-07-16 17:29:27 -0300

    Inserting template inheritance

--- digital_menu/core/urls.py
+++ digital_menu/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from digital_menu.core.views import home
+from digital_menu.core.views import home, about
 
 app_name = "core"
 urlpatterns = [
     path("", view=home, name="home"),
+    path("about/", view=about, name="about"),
 ]
