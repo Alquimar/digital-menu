a376ca2fefb92133564347093d530ce271d41f9c
Alquimar <alquimars@gmail.com>
2021-07-16 10:36:35 -0300

    Creating templates. Add index.html, css and js

--- digital_menu/core/views.py
+++ digital_menu/core/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Teste")
+    return render(request, 'core/index.html')
